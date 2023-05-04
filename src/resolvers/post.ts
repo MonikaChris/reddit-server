fe9e4e06c3a69aaacb106ca6cbe0b3497a1cbc53
Monika <monidavies@gmail.com>
2023-05-03 23:12:32 -0700

    Adds get single post query

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,6 +1,6 @@
 import { Post } from "../entities/Post";
 import { MyContext } from "src/types";
-import { Resolver, Query, Ctx } from "type-graphql";
+import { Resolver, Query, Ctx, Int, Arg } from "type-graphql";
 
 @Resolver()
 export class PostResolver {
@@ -8,4 +8,11 @@ export class PostResolver {
   posts(@Ctx() { em }: MyContext): Promise<Post []> {
     return em.find(Post, {});
   }
+
+  @Query(() => Post, { nullable: true})
+  post(
+    @Arg('id', () => Int) id: number,
+    @Ctx() { em }: MyContext): Promise <Post | null> {
+      return em.findOne(Post, { id })
+    }
 } 
